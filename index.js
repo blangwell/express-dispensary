01c51e0ac03abf10d7fd28e516cf2d2f58c58bb7
blangwell <b.langwell@outlook.com>
2021-07-20 22:05:10 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const PORT = process.env.PORT;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.use('/shop', require('./routes/shop'));
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
